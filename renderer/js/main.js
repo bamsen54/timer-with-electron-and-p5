c6a73dd82f4d4e9b1d578510bc729684f1861099
bamsen54 <149212781+bamsen54@users.noreply.github.com>
2025-05-28 15:27:14 +0200

    alarm continues in help

---
+++ renderer/js/main.js
@@ -0,0 +1,390 @@
+let alarm_sound; // wav file as alarm when count down is over
+let volume;
+
+function preload() {
+
+    alarm_sound        = document.getElementById('alarm');
+    volume = parseFloat(localStorage.getItem('alarm volume')) / 100; 
+
+    
+    alarm_sound.volume = volume;
+}
+
+
+// input      : when the user inputs the text into the input
+// count down : when the user has started the alarm and the program counts down
+// expired    : when the alarm has reached 0 and the alarm sound is played  
+let program_status = 'input';
+//console.log('program status changed to input')
+
+const title        = document.getElementById('title-text');
+const input_field  = document.getElementById('input');
+const start_button = document.getElementById('start');
+
+
+
+
+const ongoing_alarm = localStorage.getItem('ongoing-alarm');
+
+/* DEALS WITH ALARM */
+
+    // the unix timestamp when alarm should go off
+    // if current unix time stamp is x then alarm
+    // should go off at x + input_field to seconds
+    let unix_time_alarm; 
+    let unix_at_alarm_go_off;
+
+    let alarm_input_is_valid = false; // uses parser.parse_input and if it gives a valid time back this is set to true
+    
+
+    
+
+/* DEALS WITH GUI (what users sees but does not interact with)*/
+
+    let time_when_alarm_will_go_off = '';  
+
+    // displays time left of alarm
+    const count_down_text             = document.getElementById('count-down-span');
+
+    // displays date and time when alarm will be expired
+    const count_down_finished_at_span = document.getElementById('count-down-finished-at-span');
+
+    // thanks to Oriol
+    // https://stackoverflow.com/questions/22559830/html-prevent-space-bar-from-scrolling-page
+    /* window.addEventListener('keydown', (e) => { 
+        
+        if(e.key == ' ' && e.target == document.body)
+             e.preventDefault();
+    }) */
+
+
+function setup() {
+
+    create_background_canvas();
+    
+    use_help_input();
+
+    const ongoing_alarm = localStorage.getItem('ongoing-alarm');
+    
+    if( ongoing_alarm == 'none' ) {
+
+        // all of these start off as hidden so that going back to timer does not show then if not in none
+        title.className = '';
+        input_field.className = '';
+        start_button.className = 'invalid';
+    }
+
+    else if( ongoing_alarm == 'expired' ) {
+
+        program_status = 'expired';
+        //console.log('program status changed to expired')
+
+
+        input_field.style.display = 'none';
+        start_button.style.display = 'none';
+        title.style.display = 'none';
+
+        alarm_sound.muted = false;
+        alarm_sound.play();
+    }
+
+    else {
+
+        const alarm_information = JSON.parse(ongoing_alarm);
+
+        const unix_timer_info_as_integer = parseInt(alarm_information.unix_time_alarm_info);
+
+        const seconds_left = floor( (unix_timer_info_as_integer - (new Date()).getTime()) / 1000 ) ;
+
+        input_field.value = seconds_left + ' seconds';
+
+        alarm_input_is_valid = true;
+        
+        start_alarm();
+        input_field.value = '';
+    } 
+}
+
+function draw() {
+
+    background(32, 30, 30);
+    
+    window_resized();
+    
+    menu();
+    count_down();
+    expired();
+
+    if( time_when_alarm_will_go_off )
+        display_when_alarm_will_go_off(time_when_alarm_will_go_off);
+
+    turn_off_alarm_expired_state_after_a_while();
+
+    alarm_sound.volume = parseFloat(localStorage.getItem('alarm volume')) / 100;
+
+}
+    
+
+// take input to start and stop alarm
+function menu() {
+
+    if( program_status == 'input') {
+
+        const alarm_seconds = parser.parse_input(input_field.value);
+
+        if( alarm_seconds >= 0 ) {
+            
+            start_button.className = 'valid';
+
+            alarm_input_is_valid = true;
+        }
+
+        else {
+            
+            start_button.className = 'invalid';
+
+            alarm_input_is_valid = false;
+        }
+    }
+}
+
+function count_down() {
+
+    if( program_status == 'count down' ) {
+
+        const seconds_left = ceil((unix_time_alarm - (new Date()).getTime()) / 1000);
+
+        if( seconds_left <= 0 && program_status == 'count down') {
+
+            program_status  = 'expired';
+            //console.log('program status changed to expired')
+
+            unix_time_alarm = null;
+
+        }
+        
+        display_count_down(seconds_left); // it will figure out the seconds left automatically.
+    }
+
+    else {
+
+        //count_down_text.textContent = '';
+        count_down_finished_at_span.textContent = '';
+
+    }
+}
+
+function expired() {
+
+    if( program_status == 'expired' )
+        display_count_down();
+}
+
+function keyPressed(event) {
+
+    const key = event.key;
+    
+    // ▶️ the user starts the timer
+    if( key == 'Enter' && alarm_input_is_valid && program_status == 'input' ) {
+
+        start_alarm();
+
+        input_field.value = '';
+    }
+
+    if( program_status == 'expired' && key == ' ') {
+        
+        program_status = 'input'; 
+        //console.log('program status changed to input')
+
+
+        alarm_sound.pause();
+
+        input_field.style.display  = '';
+        title.style.display        = '';
+        start_button.style.display = '';
+
+        title.className = '';
+        input_field.className = '';
+        start_button.className = 'invalid';
+
+        count_down_text.textContent = '';
+
+        count_down_text.style.display = 'none';
+
+        localStorage.setItem('ongoing-alarm', 'none');
+
+        unix_time_alarm = null;
+        unix_at_alarm_go_off = null;
+    }
+
+    if(program_status == 'debug' && key == ' ')
+        parser.parse_input(input_field.value);
+
+    if( key == 'r' && program_status == 'count down' ) {
+        
+        program_status = 'input';
+        //console.log('program status changed to input')
+
+
+        count_down_text.style.display = 'none';
+
+
+        input_field.style.display  = '';
+        title.style.display        = '';
+        start_button.style.display = '';
+
+        title.className = '';
+        input_field.className = '';
+        start_button.className = 'invalid';
+
+        count_down_text.textContent = '';
+        count_down_finished_at_span.textContent = '';
+
+        unix_time_alarm      = null;
+        unix_at_alarm_go_off = null;
+
+        localStorage.setItem('ongoing-alarm', 'none');
+    }
+}
+
+// ⏰ the program starts the countdown
+// by setting the unix_time_alarm to the 
+// current unix + the seconds of the alarm
+function start_alarm() {
+
+    if( program_status == 'input' && alarm_input_is_valid ) {
+
+        const input_value = input_field.value;
+        input_field.value = '';
+
+        const seconds     = parser.parse_input(input_value); // seconds that alarm runs
+
+        if( Number.isNaN(seconds) )
+            return;
+
+        // remove input field, button and title
+        input_field.style.display = 'none';
+        start_button.style.display = 'none';
+        title.style.display = 'none';
+
+        count_down_text.style.display = '';
+
+        program_status = 'count down'; // sets program state to run
+        //console.log('program status changed to count down')
+
+
+        unix_time_alarm = Date.now() + seconds * 1000; // at what unix time stamp alarm should go off
+        //console.log(unix_time_alarm - Date.now())
+
+        timer = setInterval( timer_run , 100); // actually starts alarm
+
+        //timer_go_off_has_been_calculated = false;
+
+        time_when_alarm_will_go_off = calculate_at_what_time_alarm_will_go_off();
+        console.log(time_when_alarm_will_go_off, 'here 1');
+
+        //console.log(time_when_alarm_will_go_off)
+
+        start_button.className = 'invalid';
+
+        // setting ongoing alarm in localStorage
+
+        const alarm_information = {
+
+            unix_time_alarm_info: unix_time_alarm,
+            time_when_alarm_will_go_off_info: time_when_alarm_will_go_off
+
+        };
+
+        localStorage.setItem('ongoing-alarm', JSON.stringify(alarm_information));
+
+        input_field.className = 'input-field-not-visible';
+        
+
+    
+        return alarm_information;
+    }
+}
+
+function alarm_go_off() {
+
+    program_status = 'expired';
+    //console.log('program status changed to expired')
+
+
+    localStorage.setItem('ongoing-alarm', 'expired');
+    
+    unix_at_alarm_go_off = (new Date()).getTime();
+
+    if( input_field.style.display != '') {
+        alarm_sound.muted = false;
+        console.log('play');
+        alarm_sound.play();
+    }
+        
+    //alarm_sound.play();
+}
+
+// we set an interfval with this function
+// this function runs every x seconds
+// once the alarm has started
+// then this function checks
+// if the time is out
+// if it is the alarm
+// goes off
+function timer_run() {
+
+    // ✅ since unix_time_alarm is set
+    // to the number when unix will 
+    // be unix at start + seconds the 
+    // alarm will run, this means that
+    // the alarm has expired
+    if( Date.now() >= unix_time_alarm && unix_time_alarm ) {
+        
+        clearInterval(timer);
+        alarm_go_off();
+        
+    }
+}
+
+function turn_off_alarm_expired_state_after_a_while() {
+
+    if( program_status != 'expired' )
+        return;
+
+    const now = new Date();
+
+    let max_seconds_of_expired_time;
+    const expired_time_limit = localStorage.getItem('expired time limit');
+
+    if( expired_time_limit == 'unlimited' )
+        return;
+
+    max_seconds_of_expired_time = parseInt(expired_time_limit);
+    
+    if( unix_at_alarm_go_off ) {
+
+        const seconds_since_alarm_went_off = floor( (now - unix_at_alarm_go_off) / 1000 );
+        
+        if( seconds_since_alarm_went_off >= max_seconds_of_expired_time ) {
+
+            console.log('alarm expired limit hit')
+
+            program_status = 'input'; 
+            //console.log('program status changed to input')
+            
+            alarm_sound.pause();
+
+            input_field.style.display  = '';
+            input_field.className      = '';
+            title.style.display        = '';
+            start_button.style.display = '';
+
+            count_down_text.textContent = '';
+
+            unix_at_alarm_go_off = null;
+        }
+    }
+}
+
---
+++ renderer/js/parse-input-util.js
@@ -0,0 +1,245 @@
+// e.g. calculates seconds to 15:45 and 
+// 'x seconds' where x is the seconds to 15:45
+function calculate_seconds_until_hhmm(hhmm) {
+
+    // e.g. hhmm = '15:45' -> hh = 15, mm = 45 
+    hh = parseInt( hhmm.slice(0, 2) );
+    mm = parseInt( hhmm.slice(3, 5) );
+
+    if( hh < 0 || hh > 24 || mm < 0 || mm > 59)
+        return 'Invalid hh:mm format';
+
+    const now = new Date();
+
+    const hour_now    = now.getHours();
+    const minute_now  = now.getMinutes();
+    const seconds_now = now.getSeconds();
+
+    // difference between hhmm and now
+    // we need to multiply hour difference by 3600 and minute difference by 60
+    // since one hour is 3600 seconds and one minute is 60 seconds
+    let difference = 3600 * (hh - hour_now)  + 60 * (mm - minute_now) + (0 - seconds_now);
+
+    if( difference < 0)    
+        difference += 86400;
+
+    return difference + ' seconds ';
+}
+
+function is_leap_year(year) {
+
+    let is_leap_year = false;
+    
+    if( year % 4 == 0 )
+        is_leap_year = true;
+
+    if( year % 100 == 0 )
+        is_leap_year = false;
+
+    if( year % 400 == 0)
+        is_leap_year = true;
+
+    return is_leap_year;
+}
+
+function get_next_leap_year() {
+
+    const now = new Date();
+
+    let year = now.getFullYear();
+
+    let is_leap_year = false;
+
+    while( !is_leap_year ) {
+
+        if( year % 4 == 0 )
+            is_leap_year = true;
+
+        if( year % 100 == 0)
+            is_leap_year = false;
+
+        if( year % 400 == 0 )
+            is_leap_year = true;
+
+        if( is_leap_year )
+            break;
+
+        year++;
+    }
+
+    return year;
+}
+
+
+function combine_sign_series(plus_minus_serie) {
+
+    // we only care about minus count since 
+    // it is the only thing that change the 
+    // total sign
+    let minus_count = 0;
+
+    for( const sign of plus_minus_serie ) {
+
+        if( sign == '-' )
+            minus_count++;
+    }
+
+    return (minus_count % 2) ? '-' : '+';
+}
+
+function check_if_date_is_valid(year, month, day) {
+
+    if( Number.isNaN(year) || Number.isNaN(month) || Number.isNaN(day) )
+        return false;
+
+    if( day < 1 )
+        return false;
+    if( month > 11 )
+        return false;
+
+    if( is_leap_year(year) && month == 1 && day == 29) // leap day
+        return true;
+
+    if( day <= days_in_month[month] )
+        return true;
+
+    return false;
+}
+
+// months are 0 indexed, so January = 1, February = 2, ...
+// day of the month are 1 indexed
+// returns x + ' seconds' where x is amount of
+// seconds to the date year-month-date
+function get_seconds_to_date(year, month, day) {
+   
+    if( !check_if_date_is_valid(year, month, day ) )
+        return 'invalid date';
+    
+    const now = new Date();
+
+    const date = new Date();
+
+    // we want this year month and day at 00:00:00
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+
+    date.setFullYear(year);
+    date.setMonth(month);
+    date.setDate(day);
+
+    const ms_difference     = date - now;
+    const second_difference = ceil(ms_difference / 1000);
+
+    const time_zone_hour_offset = (date.getTimezoneOffset() - now.getTimezoneOffset()) / 60;
+
+    return second_difference;// - time_zone_hour_offset * 3600;
+}
+
+// identifies all strings on form year-month-day
+// year-month-day
+// year.month.day
+// year/month/day
+// e.g. 2025.05.03 or 2025.5.3
+function get_all_date_sub_strings(input_string) {
+    
+    // separation works with .-/
+    const dot_regex   = new RegExp('\\d+.\\d{1,2}.\\d{1,2}', 'g');
+    const dash_regex  = new RegExp('\\d+-\\d{1,2}-\\d{1,2}', 'g');
+    const slash_regex = new RegExp('\\d+/\\d{1,2}/\\d{1,2}', 'g');
+
+    let matches = [];
+    
+    matches = matches.concat([...input_string.matchAll(dot_regex)]);
+    matches = matches.concat([...input_string.matchAll(dash_regex)]);
+    matches = matches.concat([...input_string.matchAll(slash_regex)]);
+
+    for( let k = 0; k < matches.length; k++ )
+        matches[k] = matches[k][0];
+
+    // bug, matches contain duplicates
+    // this fixes it
+    return [...new Set(matches)];
+}
+
+// a string_date is one string with year month and day
+function check_if_string_date_is_valid(string_date) {
+
+    let separator = '/';
+
+    if( string_date.includes('.') )
+        separator = '.';
+
+    else if( string_date.includes('-') )
+        separator = '-';
+
+    let [year, month, day] = string_date.split(separator);
+
+    // month minus one because we 0 index months but input is 1 indexed
+    [year, month, day] = [parseInt(year), parseInt(month) - 1, parseInt(day)];
+
+    return check_if_date_is_valid(year, month, day);
+}
+
+function string_date_to_seconds(string_date) {
+
+    if( !check_if_string_date_is_valid(string_date) )
+        return 'invalid string date';
+
+    let separator = '/';
+
+    if( string_date.includes('.') )
+        separator = '.';
+
+    else if( string_date.includes('-') )
+        separator = '-';
+
+    let [year, month, day] = string_date.split(separator);
+    [year, month, day]     = [parseInt(year), parseInt(month) - 1, parseInt(day)];
+
+    return get_seconds_to_date(year, month, day) + ' seconds';
+}
+
+// takes integer 0-6
+function get_seconds_to_week_day(weeK_day) {
+
+    const now           = new Date();
+    const this_week_day = now.getDay();
+
+    const date = new Date();
+
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+
+    let days_to_week_day = weeK_day - this_week_day;
+    
+    if( days_to_week_day < 1)
+        days_to_week_day += 7;
+
+    //print( days_to_week_day);
+
+    date.setDate(date.getDate() + days_to_week_day );
+
+    return floor((date - now) / 1000);
+}
+
+// inputs that do not belong to any other category
+function misc(input_string) {
+
+    input_string = input_string.replaceAll('tomorrow', '00:00');
+    input_string = input_string.replaceAll('fortnight', '14days');
+    input_string = input_string.replaceAll('noon', '12:00');
+    input_string = input_string.replaceAll('midnight', '00:00');
+
+    return input_string;
+}
+
+// thanks to 
+// Saranga Jayaruwan
+// https://stackoverflow.com/questions/1779013/check-if-string-contains-only-digits
+function is_a_number_string(string){
+
+    return Number.isInteger(parseInt(string));
+}
+
